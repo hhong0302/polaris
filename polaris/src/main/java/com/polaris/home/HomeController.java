6c6db5e70e852632c2b882968991d71ad45bba13
hong <124054037+hhong0302@users.noreply.github.com>
2023-06-19 12:26:42 +0900

    command

--- polaris/src/main/java/com/polaris/home/HomeController.java
+++ polaris/src/main/java/com/polaris/home/HomeController.java
@@ -1,15 +1,18 @@
 package com.polaris.home;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.polaris.home.command.SpCommand;
+import com.polaris.home.util.Static;
 
 /**
  * Handles requests for the application home page.
@@ -18,6 +21,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	//모든 command가 갖고 있는 인터페이스 타입을 선언
+		SpCommand command;
+			
+		//jdbc Spring template
+		public JdbcTemplate template;
+		
+		@Autowired
+		public void setTemplate(JdbcTemplate template)
+		{
+			this.template = template;
+			Static.template=this.template;
+		}
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
